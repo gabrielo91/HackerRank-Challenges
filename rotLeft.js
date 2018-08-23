4cf4f38b873e69ab8581dc70dd5c588eae090620
Gabriel Ortega Rosero <gabrielo_9103@hotmail.com>
2018-08-23 00:34:47 -0500

    Improve answer rotate left challenge
    https://www.hackerrank.com/challenges/ctci-array-left-rotation

--- rotLeft.js
+++ rotLeft.js
@@ -1,25 +1,13 @@
+// Complete the rotLeft function below.
 function rotLeft(a, d) {
-    console.log('a')
-    console.log(a)
-    console.log('d')
-    console.log(d)
     let shifts = d
     let rotatedArray = []
     const arraySize = a.length
     if (shifts > arraySize) {
         shifts = shifts%arraySize
     }
-    for(let i=0; i<shifts; i++){
-        rotatedArray = rotate(a)
-        a = rotatedArray
-    }
-    
-    return rotatedArray
-}
-
-function rotate(arr){
-    const firstValue = arr[0]
-    let rotatedArray = arr.slice(1, arr.length)
-    rotatedArray.push(firstValue)
-    return rotatedArray
+    let rotatedValues = a.slice(0, shifts)
+    let slicedArray = a.slice(shifts, a.length)
+    a = slicedArray.concat(rotatedValues)
+    return a
 }
