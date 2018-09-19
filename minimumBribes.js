33787e3f22a5092aff4e13ed1ab05de2aff1fd9d
Gabriel Ortega Rosero <gabrielo_9103@hotmail.com>
2018-09-18 23:31:20 -0500

    Update minimumBribes.js
    Final soluction with inversion array solution problem approach

--- minimumBribes.js
+++ minimumBribes.js
@@ -1,19 +1,22 @@
 function minimumBribes(q) {
     let result = 0;
-    for (let position =0; position < q.length; position++){
-        let currentValue = q[position]
-        let minPositionValue = currentValue-2
-        let maxPositionValue = currentValue+2
-        let currentPosition = position + 1
-        
-        if(currentPosition<minPositionValue){
-            result = 'Too chaotic';
-            break;
-        } else if (currentValue > currentPosition) {
-            result = result + currentValue - currentPosition
+    let index = 0;
+    let cuenta = 0;
+    for (let value in q){
+        index++
+        let remains = q.slice(index, q.length)
+        let internalCount = 0;
+        for (let remain in remains){
+           if (q[value] > remains[remain]){
+               internalCount++;
+               cuenta++;
+               if (internalCount > 2) {
+                   console.log('Too chaotic')
+                   return('Too chaotic')
+               }
+           }
         }
     }
-    console.log(result)
-    return result
-
+    console.log(cuenta)
+    return cuenta
 }
